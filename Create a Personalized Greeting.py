9f8589fa489a0089cd9d78aa2ab2ab2ea686dc93
Mohkumak <mohitkumarurmila@gmail.com>
2025-07-06 20:58:16 +0530

    Add files via upload

---
+++ Create a Personalized Greeting.py
@@ -0,0 +1,13 @@
+# Personalized Greeting....
+
+First_name = input(str("Enter the first name: "))
+Last_name = input(str("Enter the last name: "))
+Full_name = First_name + " " + Last_name
+print(f"Hello, {Full_name}! Welcome to the python program.")
+
+
+
+
+
+
+
